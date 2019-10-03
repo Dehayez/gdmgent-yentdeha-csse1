231259eb12cea33c029eb6818d45571844349b9e
Yentel De Hauwere <tim.depaepe@arteveldehs.be>
2019-10-03 13:58:29 +0200

    WIP

---
+++ Lessen/Les01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Les01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
